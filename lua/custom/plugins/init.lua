2660299e0e23903d9711dc46da6bdfaab3b8895c
Dmitry O <feanarokurufinve@gmail.com>
2024-09-15 22:59:01 +0200

    added leetcode.nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -295,4 +295,25 @@ return {
       'rcarriga/nvim-notify',
     },
   },
+  {
+    'kawre/leetcode.nvim',
+    build = ':TSUpdate html',
+    dependencies = {
+
+      'nvim-telescope/telescope.nvim',
+      'nvim-lua/plenary.nvim', -- required by telescope
+      'MunifTanjim/nui.nvim',
+
+      -- optional
+      'nvim-treesitter/nvim-treesitter',
+      'rcarriga/nvim-notify',
+      'nvim-tree/nvim-web-devicons',
+    },
+    cmd = 'Leet',
+    opts = {
+      plugins = {
+        non_standalone = true,
+      },
+    },
+  },
 }
