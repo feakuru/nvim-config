4d1d0b9a81261fcd578cad6bf4894c313e51aad7
Dmitry O <feanarokurufinve@gmail.com>
2024-09-23 00:41:22 +0200

    using ~/.local/bin/mypy instead of /usr/bin/mypy

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -65,7 +65,7 @@ return {
     ft = { 'python' },
     opts = function()
       local null_ls = require 'null-ls'
-      local virtual = os.getenv 'VIRTUAL_ENV' or os.getenv 'CONDA_PREFIX' or '/usr'
+      local virtual = os.getenv 'VIRTUAL_ENV' or os.getenv 'CONDA_PREFIX' or os.getenv 'HOME' .. '/.local'
       return {
         sources = {
           null_ls.builtins.diagnostics.mypy.with {
