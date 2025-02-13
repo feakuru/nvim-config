19de16404fd90481f4236bc84b2079407dadd2a5
Dmitry O <feanarokurufinve@gmail.com>
2025-02-13 18:16:39 +0100

    fix dropbar breaking blame line match

--- lua/custom/plugins/dropbar.lua
+++ lua/custom/plugins/dropbar.lua
@@ -10,6 +10,21 @@ return {
       vim.keymap.set('n', '<Leader>;', dropbar_api.pick, { desc = 'Pick in Dropbar' })
       vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Goto start of current context' })
       vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
-    end,
+      local default_config = require 'dropbar.configs'.opts
+      require('dropbar').setup {
+        bar = {
+          enable = function(buf, win)
+            return default_config.bar.enable(buf, win)
+              or vim.bo[buf].ft == 'fugitiveblame'
+          end,
+          sources = function(buf, win)
+            if vim.bo[buf].ft == 'fugitiveblame' then
+              return {}
+            end
+            return default_config.bar.sources(buf, win)
+          end
+        },
+      }
+    end
   },
 }
