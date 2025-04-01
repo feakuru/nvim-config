556833fca3e52dc2d9507bbd362d0038a0785c8d
Dmitry O <feanarokurufinve@gmail.com>
2025-04-01 11:08:39 +0200

    uncomment new window move keymaps

--- init.lua
+++ init.lua
@@ -231,10 +231,10 @@ vim.keymap.set('n', '<leader>tt', '<Cmd>vsplit<CR><Cmd>term<CR>', { desc = '[T]o
 vim.keymap.set('n', '<leader>tT', '<Cmd>split<CR><Cmd>term<CR>', { desc = '[T]oggle [T]erminal down' })
 
 -- NOTE: Some terminals have coliding keymaps or are not able to send distinct keycodes
--- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
--- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
--- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
--- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
+vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
+vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
+vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
+vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
