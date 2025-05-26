9c6ae8cbd3251e551c66b76ad06db9ff2c7eaf14
Dmitry O <feanarokurufinve@gmail.com>
2025-05-26 11:42:41 +0200

    disable mypy on startup by default

--- lua/custom/plugins/mypy.lua
+++ lua/custom/plugins/mypy.lua
@@ -2,8 +2,11 @@ return {
   {
     'feakuru/mypy.nvim',
     config = function()
-      require('mypy').setup()
-      vim.keymap.set('n', '<leader>mt', '<Cmd>MypyToggle<CR>', {desc = "[M]ypy [T]oggle"})
+      local mypy = require 'mypy'
+      mypy.setup()
+      mypy.enabled = false
+
+      vim.keymap.set('n', '<leader>mt', '<Cmd>MypyToggle<CR>', { desc = '[M]ypy [T]oggle' })
     end,
   },
 }
