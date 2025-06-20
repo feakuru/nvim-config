da7d72e9786931f58d15a833c42013894918be50
Dmitry O <feanarokurufinve@gmail.com>
2025-06-20 19:12:56 +0200

    finegrained cache option for mypy

--- lua/custom/plugins/mypy.lua
+++ lua/custom/plugins/mypy.lua
@@ -4,7 +4,9 @@ return {
     -- dir = "~/projects/public/mypy.nvim/",
     config = function()
       local mypy = require 'mypy'
-      mypy.setup()
+      mypy.setup {
+        extra_args = { '--cache-fine-grained' },
+      }
       mypy.enabled = false
 
       vim.keymap.set('n', '<leader>mt', '<Cmd>MypyToggle<CR>', { desc = '[M]ypy [T]oggle' })
