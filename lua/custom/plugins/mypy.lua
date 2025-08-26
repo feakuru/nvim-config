80370954c6c243698382965cc0120e1561f7a828
Dmitry O <feanarokurufinve@gmail.com>
2025-08-26 21:07:19 +0200

    Enable mypy by default

--- lua/custom/plugins/mypy.lua
+++ lua/custom/plugins/mypy.lua
@@ -1,14 +1,11 @@
 return {
   {
     'feakuru/mypy.nvim',
-    -- dir = "~/projects/public/mypy.nvim/",
     config = function()
       local mypy = require 'mypy'
       mypy.setup {
         extra_args = { '--cache-fine-grained' },
       }
-      mypy.enabled = false
-
       vim.keymap.set('n', '<leader>mt', '<Cmd>MypyToggle<CR>', { desc = '[M]ypy [T]oggle' })
     end,
   },
