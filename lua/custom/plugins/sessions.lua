6c6b920f177dd53b9f3bf8ab073ef4046e114068
Dmitry O <feanarokurufinve@gmail.com>
2025-09-16 22:42:22 +0200

    Update session delete command

--- lua/custom/plugins/sessions.lua
+++ lua/custom/plugins/sessions.lua
@@ -8,7 +8,7 @@ return {
       require('auto-session').setup {
         auto_session_suppress_dirs = { '~/', '~/projects', '~/Downloads', '/' },
       }
-      vim.keymap.set('n', '<leader>ds', '<Cmd>SessionDelete<CR>', { desc = '[D]elete [S]ession' })
+      vim.keymap.set('n', '<leader>ds', '<Cmd>AutoSession delete<CR>', { desc = '[D]elete [S]ession' })
     end,
   },
 }
